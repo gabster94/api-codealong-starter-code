c9f791040c6a13f2115450c1267a45b9e062031c
gabster94 <iofe.gabriella@gmail.com>
2024-09-26 13:50:50 +0200

    code along half way. not finished

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("🌻 Script connected")
+// console.log("🌻 Script connected")
+// 9a4807cb-1e38-4de9-bb9c-5b732a84a6c9
+// https://www.dictionaryapi.com/api/v3/references/thesaurus/json/umpire?key=your-api-key
+// URL = base url + word + api_key
+
+const API_KEY = "9a4807cb-1e38-4de9-bb9c-5b732a84a6c9"
+const BASE_URL = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json"
+const word = "umpire"
+
+const URL = `${BASE_URL}/${word}?key=${API_KEY}`
+
+console.log(URL)
+
+//DOM selectors
+const wordTitle = document.getElementById("word")
+
+wordTitle.innerHTML = word
+
+
+fetch(URL)
+  .then(response => response.json()) 
+  .then(data => {
+    console.log(data)
+
+    
+})
